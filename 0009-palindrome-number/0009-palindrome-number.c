b57594b38a23115803a75071af93f752e4398dc6
Arnav Agarwal <arnav.link.git@gmail.com>
2026-08-26 17:35:55 +0530

    Time: 0 ms (100%), Space: 8.9 MB (22.37%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.c
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -1,14 +1,18 @@
-bool isPalindrome(int x) {
-    if (x<0)
-        return false;
+#include <stdbool.h>
 
-    int d,n=x;
-    long int sum=0;
-    while (x!=0){
-        d=x%10;
-        sum=(sum*10)+d;
-        x=x/10;
+bool isPalindrome(int x) {
+    // Negative numbers are never palindromes
+    if (x < 0) return false;
+
+    // Reversed number could overflow int, so use long
+    int n = x;
+    long sum = 0;
+
+    while (x != 0) {
+        int d = x % 10;
+        sum = sum * 10 + d;
+        x = x / 10;
     }
 
-    return (sum == n);
+    return sum == n;
 }
